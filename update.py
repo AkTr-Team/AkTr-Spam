c373aa2e327bc432356c04e00146e8531ccce02d
AkTr_ <90263361+AkTr-Team@users.noreply.github.com>
2021-11-28 20:31:55 -0300

    Update and rename updates.py to update.py

---
+++ update.py
@@ -0,0 +1,19 @@
+import os
+
+os.system('rm -rf agradecimentos.py')
+
+os.system('rm -rf gpzpzp.py')
+
+os.system('rm -rf info.py')
+
+os.system('rm -rf ss.py')
+
+os.system("rm -rf '#comandos'")
+
+os.system('rm -rf #comandos')
+
+os.system('rm -rf git pull')
+
+print('PRONTO, VERIFICAÇÃO DE ATUALIZAÇÃO FINALIZADA.')
+
+#BY AKTR:)
