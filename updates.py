76f1976fcdee4f3ce3628bc07c6f3e184e40d94b
AkTr_ <90263361+AkTr-Team@users.noreply.github.com>
2021-11-28 20:27:09 -0300

    Add files via upload

---
+++ updates.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('agradecimentos.py')
+
+os.system('gpzpzp.py')
+
+os.system('info.py')
+
+os.system('ss.py')
+
+os.system("'#comandos'")
+
+os.system('#comandos')
+
+os.system('git pull')
